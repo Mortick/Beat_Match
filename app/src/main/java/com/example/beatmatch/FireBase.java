93946b107c63ec5bd1b8dc8d3a6bde85da712fc5
mortick <qing.ch156@gmail.com>
2019-03-02 11:54:36 -0500

    Added Firebase

--- app/src/main/java/com/example/beatmatch/FireBase.java
+++ app/src/main/java/com/example/beatmatch/FireBase.java
@@ -1,4 +1,16 @@
 package com.example.beatmatch;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FireBase {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+
+    public FireBase() {
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference("beatmatch-1c911");
+
+    }
 }
