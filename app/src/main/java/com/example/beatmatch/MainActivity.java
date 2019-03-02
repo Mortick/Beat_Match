8b20d4861c9677def2f389aed06ffc0feaa3bc6f
mortick <qing.ch156@gmail.com>
2019-03-02 11:33:13 -0500

    Added Firebase

--- app/src/main/java/com/example/beatmatch/MainActivity.java
+++ app/src/main/java/com/example/beatmatch/MainActivity.java
@@ -12,12 +12,21 @@ import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.types.Track;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String CLIENT_ID = "0cdf3e0de4db494c8632548161915b48";
     private static final String REDIRECT_URI = "beatmatch://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
     private Button accelerometerButton;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
 
     private Button mPlay;
@@ -120,4 +129,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    /******
+     *
+     *
+     *
+     *
+     */
+
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("stuff", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("stuff", "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
+    }
+
 }
