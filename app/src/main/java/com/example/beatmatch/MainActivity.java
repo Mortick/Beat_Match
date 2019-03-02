02feba13ffc4a6468cf92e76fa9bd740ce6d2034
Mortick <qing.ch156@gmail.com>
2019-03-01 19:44:31 -0500

    Imported spotify

--- app/src/main/java/com/example/beatmatch/MainActivity.java
+++ app/src/main/java/com/example/beatmatch/MainActivity.java
@@ -3,11 +3,35 @@ package com.example.beatmatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
     }
 }
