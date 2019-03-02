439f3d3866964fea0c6ac8b58c52824dc3d8505b
Lucy Chen <yic76@pitt.edu>
2019-03-02 14:50:44 -0500

    added stop btn

--- app/src/main/java/com/example/beatmatch/MainActivity.java
+++ app/src/main/java/com/example/beatmatch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.beatmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mPause;
     private Button mPlay;
+    private Button mStop;
     private Chronometer chronometer;
 
     //I added this
@@ -85,6 +87,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mSpotifyAppRemote.getPlayerApi().pause();
+            }
+        });
+        mStop = (Button) findViewById(R.id.stopbtn);
+        mStop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mSpotifyAppRemote.getPlayerApi().pause();
+                startActivity (new Intent(MainActivity.this, HomePage.class));
             }
         });
     }
